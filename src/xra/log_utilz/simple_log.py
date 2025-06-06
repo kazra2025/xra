a429c307d2a62046fa1952c50d0384395ad1eb38
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-06 01:44:16 -0400

    log utilz timestamp improved

--- src/xra/log_utilz/simple_log.py
+++ src/xra/log_utilz/simple_log.py
@@ -13,7 +13,10 @@ def _process_log_record(lgr: LOG_RECORD):
     # func_name = lgr.func_name
     fbasename = os.path.basename(lgr.filename)
 
-    msg_builder = f"{int(lgr.lgr_time_ns)}|{fbasename}:{lgr.line_no}|{lgr.msg}"
+    # lgr.lgr_time_ns is nanosecond. we need less.
+    time_seconds = round(lgr.lgr_time_ns / 1_000_000_000, 2)  # convert to seconds
+    time_trunc = f"{time_seconds:.3f}"[6:]
+    msg_builder = f"{time_trunc}|{fbasename}:{lgr.line_no}|{lgr.msg}"
 
     if lgr.msg_lvl == LGLVL.DBUG:
         msg_builder = f'DBUG|{msg_builder}'
