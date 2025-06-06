42102088c807930c4b5d926b039033906bf71d44
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-06 01:26:45 -0400

    main2

---
+++ src/main2.py
@@ -0,0 +1,31 @@
+import os
+import io
+import sys
+import json
+import random
+import hashlib
+import gc
+import traceback
+import subprocess as sp
+from pathlib import Path
+import shutil
+
+from xra.central_config import dfcc
+# from xra.log_utilz.log_man import current_logger as log
+
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------
+def main():
+    print("\n\n")
+    # log.info("main")
+    print(f"repo_root: {dfcc.repo_root}")
+    print("\n\n")
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------
+if '__main__' == __name__:
+    main()
